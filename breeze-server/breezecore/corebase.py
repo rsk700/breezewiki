3ce05148c0b6e76d7c1b18a6f1ca96c2610cf009
rsk <rskbox@gmail.com>
2018-03-21 08:02:54 +0300

    use base class for core objects (files)

---
+++ breeze-server/breezecore/corebase.py
@@ -0,0 +1,11 @@
+class CoreBase(object):
+
+    """Docstring for CoreBase. """
+
+    def __init__(self, session):
+        """TODO: to be defined1.
+
+        :session: TODO
+
+        """
+        self.s = session
